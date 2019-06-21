723e99f481ac6b251be95a30f49af04c6d096e6f
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-21 15:52:49 -0700

    First Commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-var express = require('express');
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios')
 var app = express();
 
 // this adds some logging to each request
+app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+  res.render('home');
+});
+
+app.get('/movies', function(req, res) {
+  axios.get('https://www.omdbapi.com/?s=' + req.query.movie + '&apikey='+ process.env.OMDB_KEY)
+  .then(function(result) {
+    res.render('movie', {movies: result.data.Search});
+  });
+});
+
+app.get('/movies/:id', function(req, res) {
+  axios.get('https://www.omdbapi.com/?i=' + req.params.id + '&apikey='+ process.env.OMDB_KEY)
+  .then(function(result) {
+    //res.json(result.data);
+    res.render('details', {movie: result.data});
+  });
 });
 
 var server = app.listen(process.env.PORT || 3000);
